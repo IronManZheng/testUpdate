745b03b93fb09af4a3e5453ae3f97b6158df396f
unknown <lovebreakintao@163.com>
2018-11-22 22:59:56 +0800

    first commit

---
+++ my_mouse_control.py
@@ -0,0 +1,8 @@
+import pyautogui
+
+#pyautogui.moveTo(100,150)
+
+pyautogui.moveTo(850,1050,duration=2)
+pyautogui.mouseDown(button='left')
+pyautogui.mouseUp(button='left')
+#pyautogui.click(850,1050,button='left')
